01418d686012b50708a17cf1d7274995926909af
NastyFever <k-lime@hotmail.com>
2014-11-03 15:35:49 +0100

    You can now chose item in the model

--- src/model/ExcelBridge.java
+++ src/model/ExcelBridge.java
@@ -2,6 +2,8 @@ package model;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.FileNotFoundException;
+
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -15,26 +17,48 @@ public class ExcelBridge {
     final static int SECOND_COLUMN = 1;
     final static int THIRD_COLUMN = 2;
     
-    private Items items = new Items();
+    private boolean endOfColumns = false;
+    
+    public Items items = new Items();
 	
-	public ExcelBridge (String file) throws BiffException, IOException{
+	public ExcelBridge (String file) throws BiffException{
 		
+	  Workbook workbook;
+	  try {
+		workbook = Workbook.getWorkbook(new File("Test.ods"));
+
+		Sheet sheet = workbook.getSheet(0);
+		  
+		for(int i=0; i< sheet.getColumn(FIRST_COLUMN).length; i++){
+//			String output = "Row: " + (i+1) + " ";
+//			System.out.println(output);
+			Cell cell1 = sheet.getCell(FIRST_COLUMN, i);
+			Cell cell2 = sheet.getCell(SECOND_COLUMN, i);
+			Cell cell3 = sheet.getCell(THIRD_COLUMN, i);
+			
+			if(endOfColumns){
+				continue;
+			}
+			
+			Item item = new Item(new Integer(cell1.getContents()), (cell2.getContents()), 
+					  new Integer(cell3.getContents()));
+			items.addItem(item);
+			//		          output += cell1.getContents() + " " + cell2.getContents();
+		    System.out.println(item.print());
+		}
+		workbook.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldn't find file");
+		}
+		catch (java.lang.NumberFormatException e) {
+			endOfColumns = true;
+		}
 		
-		Workbook workbook = Workbook.getWorkbook(new File("Test.ods"));
-      Sheet sheet = workbook.getSheet(0);
-	   
-      for(int i=0; i< sheet.getColumn(FIRST_COLUMN).length; i++){
-    	  String output = "Row: " + (i+1);
-          Cell cell1 = sheet.getCell(FIRST_COLUMN, i);
-          Cell cell2 = sheet.getCell(SECOND_COLUMN, i);
-          Cell cell3 = sheet.getCell(THIRD_COLUMN, i);
-          items.addItem(new Item(new Integer(cell1.getContents()), (cell2.getContents()), 
-        		  new Integer(cell3.getContents())));
-//          output += cell1.getContents() + " " + cell2.getContents();
-          
-          System.out.println(output);		   
-	  }
-   
-      workbook.close();
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+	
 }
--- src/model/MainClass.java
+++ src/model/MainClass.java
@@ -2,6 +2,7 @@ package model;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -18,40 +19,36 @@ import jxl.write.WriteException;
 public class MainClass {
     final static int FIRST_COLUMN = 0;
     final static int SECOND_COLUMN = 1;
-   public static void main(String[] args) 
+
+    public enum State {ITEM_INPUT, USER_INPUT};
+    
+    static State state = State.ITEM_INPUT;
+    
+    static Item item;
+    
+    public static void main(String[] args) 
 		      throws BiffException, IOException, WriteException
 	   {
-//	      WritableWorkbook wworkbook;
-//	      wworkbook = Workbook.createWorkbook(new File("Test.ods"));
-//	      WritableSheet wsheet = wworkbook.createSheet("First Sheet", 0);
-//	      
-//
-//	      char letter = 'A';
-//	      for(int i =0; i<26; i++) {
-//	    	  int row = i;
-//		      Label label = new Label(FIRST_COLUMN, row, "" + letter);
-//		      wsheet.addCell(label);
-//		      
-//		      Number number = new Number(SECOND_COLUMN, row, i+1);
-//		      wsheet.addCell(number);
-//	    	  letter++;
-//	      }
-//	     ne
-//	      
-//	      
-//	      
-//	      wworkbook.write();
-//	      wworkbook.close();
-	   new ExcelBridge("");
-//	   testMethodReadSome();
-	   
-//		      Workbook workbook = Workbook.getWorkbook(new File("Test.ods"));
-//		      Sheet sheet = workbook.getSheet(0);
-//		      Cell cell1 = sheet.getCell(0, 2);
-//		      System.out.println(cell1.getContents());
-//		      Cell cell2 = sheet.getCell(3, 4);
-//		      System.out.println(cell2.getContents());
-//		      workbook.close();
+		   System.out.println("Starting testing");
+	
+		   ExcelBridge eBridge = new ExcelBridge("");
+		   
+		   
+		   while(true) {
+			   Scanner user_input = new Scanner(System.in);
+			   
+			   String barCodeS = user_input.next();
+			   int barCode = Integer.parseInt(barCodeS);
+			   
+			   if(state.equals(State.ITEM_INPUT)) {
+				   item = eBridge.items.getItem(barCode);
+				   if(item!=null){
+					   System.out.println("HA! Vill du köpa en " + item.print());
+					   state = State.USER_INPUT;
+				   }
+			   }
+		   }
+		   
 	   }
    
    private static void testMethodReadSome() throws BiffException, IOException{
